843bf8bd588775793f7ecac8b803816db9c5bb5d
Douglas de Oliveira <oliveira.douglas@outlook.com>
2021-05-17 00:16:54 -0300

    feat: Prismic integration

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,5 +1,5 @@
 import { signIn, useSession } from 'next-auth/client';
-import { useRouter } from 'next/dist/client/router';
+import { useRouter } from 'next/router';
 
 import api from '@services/api';
 import getStripeJs from '@services/stripe-js';
@@ -18,6 +18,7 @@ export default function SubscribeButton() {
 
 		if (session?.activeSubscription) {
 			push('/posts');
+			return;
 		}
 
 		try {
